ca18fb1eed5e8cfafc8d5d631a072866f76b10ff
kulby <miha1993kul@gmail.com>
2018-06-22 10:38:09 +0300

    Added arrow, accelerometer

---
+++ Assets/Arrow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Arrow : MonoBehaviour {
+
+	private GameObject arrow;
+	private float reaction;
+	public float speed = 10.0f;
+
+	void Start() {
+		GenerateDirection ();
+	}
+
+	void Update() {
+		Vector3 dir = Vector3.zero;
+		dir.x = -Input.acceleration.y;
+		dir.z = Input.acceleration.x;
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
+
+		dir *= Time.deltaTime;
+
+		reaction = dir.z * speed;
+		//transform.Translate(dir * speed);
+		//inputDirection.text = (dir * speed).ToString();
+		if (reaction >= 0.2f) {
+			GenerateDirection ();
+		}
+	}
+
+	void GenerateDirection() {
+		transform.Rotate (0, 0, 90f);
+	}
+}
