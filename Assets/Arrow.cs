f772a2941e9d38409b7a94ec7d2339661892ea09
kulby <miha1993kul@gmail.com>
2018-06-26 20:16:44 +0300

    Added timer, directions + keyarrows, animation

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -6,11 +6,24 @@ using UnityEngine.UI;
 public class Arrow : MonoBehaviour {
 
 	private GameObject arrow;
-	private float reaction;
+	private float reactionZ;
+	private float reactionX;
 	public float speed = 10.0f;
+	private Animation arrowAnim;
+	private Dictionary<int, string> directions;
+	private string rightDirection;
+
+	private float startTime, elapsedTime;
+	private int elapsedMS;
 
 	void Start() {
-		GenerateDirection ();
+		directions = new Dictionary<int, string>(4);
+		directions.Add (0, "up");
+		directions.Add (1, "left");
+		directions.Add (2, "down");
+		directions.Add (3, "right");
+		arrowAnim = GetComponent<Animation> ();
+		NextArrow ();
 	}
 
 	void Update() {
@@ -22,15 +35,30 @@ public class Arrow : MonoBehaviour {
 
 		dir *= Time.deltaTime;
 
-		reaction = dir.z * speed;
+		reactionZ = dir.z * speed;
+		reactionX = dir.x * speed;
 		//transform.Translate(dir * speed);
 		//inputDirection.text = (dir * speed).ToString();
-		if (reaction >= 0.2f) {
-			GenerateDirection ();
+		if ((reactionZ >= 0.2f || Input.GetKeyDown (KeyCode.RightArrow)) && rightDirection == "right") {
+			NextArrow ();
+		} else if ((reactionZ <= -0.2f || Input.GetKeyDown (KeyCode.LeftArrow)) && rightDirection == "left") {
+			NextArrow ();
+		} else if ((reactionX >= 0.2f || Input.GetKeyDown (KeyCode.UpArrow)) && rightDirection == "up") {
+			NextArrow ();
+		} else if ((reactionX <= -0.2f || Input.GetKeyDown (KeyCode.DownArrow)) && rightDirection == "down") {
+			NextArrow ();
 		}
 	}
 
-	void GenerateDirection() {
-		transform.Rotate (0, 0, 90f);
+	void NextArrow() {
+		elapsedTime = Time.realtimeSinceStartup - startTime;
+		elapsedMS = Mathf.RoundToInt (elapsedTime * 1000);
+		arrowAnim.Play ("FadeOut");
+		int r = Random.Range (0, 4);
+		rightDirection = directions [r];
+		Debug.Log (elapsedMS);
+		transform.eulerAngles = new Vector3 (0f, 0f, r * 90f);
+		arrowAnim.Play ("FadeIn");
+		startTime = Time.realtimeSinceStartup;
 	}
 }
