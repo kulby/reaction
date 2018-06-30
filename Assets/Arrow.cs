c6004da4ca49d64f3c93fa892076ef98fa645f11
kulby <miha1993kul@gmail.com>
2018-06-30 13:32:13 +0300

    Accelerometer and Gyroscope used for reaction detection

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -5,60 +5,128 @@ using UnityEngine.UI;
 
 public class Arrow : MonoBehaviour {
 
+	public Text arrowsPassedText;
+	public Text debugX, debugY;
+	public bool roundStarted, roundEnded;
+
 	private GameObject arrow;
-	private float reactionZ;
-	private float reactionX;
-	public float speed = 10.0f;
 	private Animation arrowAnim;
 	private Dictionary<int, string> directions;
-	private string rightDirection;
 
+	private string rightDirection;
+	private float gyroX, gyroY, accX, accY;
 	private float startTime, elapsedTime;
 	private int elapsedMS;
+	private int curRand, lastRand;
+	private int arrowsPassed;
 
 	void Start() {
+		roundStarted = false;
+		roundEnded = false;
+		arrowsPassed = 0;
+		lastRand = 5;
 		directions = new Dictionary<int, string>(4);
 		directions.Add (0, "up");
 		directions.Add (1, "left");
 		directions.Add (2, "down");
 		directions.Add (3, "right");
 		arrowAnim = GetComponent<Animation> ();
-		NextArrow ();
+		GenerateArrow ();
 	}
 
 	void Update() {
-		Vector3 dir = Vector3.zero;
-		dir.x = -Input.acceleration.y;
-		dir.z = Input.acceleration.x;
-		if (dir.sqrMagnitude > 1)
-			dir.Normalize();
 
-		dir *= Time.deltaTime;
+		if (roundEnded == true) {
+			GenerateArrow ();
+			roundEnded = false;
+		}
 
-		reactionZ = dir.z * speed;
-		reactionX = dir.x * speed;
-		//transform.Translate(dir * speed);
-		//inputDirection.text = (dir * speed).ToString();
-		if ((reactionZ >= 0.2f || Input.GetKeyDown (KeyCode.RightArrow)) && rightDirection == "right") {
-			NextArrow ();
-		} else if ((reactionZ <= -0.2f || Input.GetKeyDown (KeyCode.LeftArrow)) && rightDirection == "left") {
-			NextArrow ();
-		} else if ((reactionX >= 0.2f || Input.GetKeyDown (KeyCode.UpArrow)) && rightDirection == "up") {
-			NextArrow ();
-		} else if ((reactionX <= -0.2f || Input.GetKeyDown (KeyCode.DownArrow)) && rightDirection == "down") {
-			NextArrow ();
+		/* ACCELEROMETR */ 
+
+		Vector3 acceleration = Input.acceleration;
+		accX = acceleration.x;
+		accY = acceleration.y;
+		
+		/* GYROSCOPE */
+
+		Quaternion gyroQaternion = Input.gyro.attitude;
+		Vector3 eulerGyro = gyroQaternion.eulerAngles;
+
+		gyroX = eulerGyro.x;
+		gyroY = eulerGyro.y;
+
+		if (gyroX > 180f) {
+			gyroX -= 360f;
+		}
+
+		if (gyroY > 180f) {
+			gyroY -= 360f;
+		}
+
+		/* DEBUG */
+
+		debugX.text = gyroX.ToString();
+		debugY.text = gyroY.ToString();
+
+		//debugX.text = accX.ToString();
+		//debugY.text = accY.ToString();
+
+		/* REACTION */
+
+		if (roundStarted) {
+			if (rightDirection == "right") {
+				if ((gyroY > 25f && accX >= 0.5f) || Input.GetKeyDown (KeyCode.RightArrow)) {
+					GoodRound ();
+				} else if (gyroX > 30f || gyroX < -10f || gyroY < -25f) {
+					GameOver ();
+				}
+			} else if (rightDirection == "left") {
+				if ((gyroY < -25f && accX <= -0.5f) || Input.GetKeyDown (KeyCode.LeftArrow)) {
+					GoodRound ();
+				} else if (gyroX > 30f || gyroX < -10f || gyroY > 25f) {
+					GameOver ();
+				}
+			} else if (rightDirection == "up") {
+				if ((gyroX < -10f && accY >= 0.5f) || Input.GetKeyDown (KeyCode.UpArrow)) {
+					GoodRound ();
+				} else if (gyroY > 20f || gyroY < -20f || gyroX > 25f) {
+					GameOver ();
+				}
+			} else if (rightDirection == "down") {
+				if ((gyroX > 25f && accY <= -0.5f) || Input.GetKeyDown (KeyCode.DownArrow)) {
+					GoodRound ();
+				} else if (gyroY > 25f || gyroY < -25f || gyroX < -5f) {
+					GameOver ();
+				}
+			}
 		}
 	}
 
-	void NextArrow() {
+	void GoodRound() {
+		roundStarted = false;
 		elapsedTime = Time.realtimeSinceStartup - startTime;
 		elapsedMS = Mathf.RoundToInt (elapsedTime * 1000);
-		arrowAnim.Play ("FadeOut");
-		int r = Random.Range (0, 4);
-		rightDirection = directions [r];
 		Debug.Log (elapsedMS);
-		transform.eulerAngles = new Vector3 (0f, 0f, r * 90f);
+		arrowsPassed++;
+		arrowsPassedText.text = arrowsPassed.ToString ();
+		arrowAnim.Play ("FadeOut");
+	}
+
+	void GenerateArrow() {
+		curRand = Random.Range (0, 4);
+		while (curRand == lastRand) {
+			curRand = Random.Range (0, 4);
+		}
+		lastRand = curRand;
+		rightDirection = directions [curRand];
+		transform.eulerAngles = new Vector3 (0f, 0f, curRand * 90f);
 		arrowAnim.Play ("FadeIn");
 		startTime = Time.realtimeSinceStartup;
+	}
+
+	void GameOver() {
+		roundStarted = false;
+		transform.localScale = new Vector3 (0f, 0f, 0f);
+		arrowsPassedText.text = "GameOver!";
 	}
 }
